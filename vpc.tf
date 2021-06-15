c4701e108f21280d0d87b4398c009a0d738c8936
vosunda <vosunda@gmail.com>
2021-06-15 08:56:56 +0000

    modified network name

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,11 @@
 module "network" {
   source  = "vosunda/network/google"
   version = "3.2.2"
-  network_name = "gaurav-network"
+  network_name = "${var.prefix}-subnet"
   project_id = "var.project"
   subnets = [
     {
-      subnet_name   = "gaurav-subnet"
+      subnet_name   = "${var.prefix}-subnet"
       subnet_ip     = "10.100.10.0/24"
       subnet_region = var.region
     }
